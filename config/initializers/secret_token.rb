46dd12eb71c7292939cf85ea5a62c15564ab763c
StrekanovaMarina <strekanova@gmail.com>
2012-12-07 11:44:30 +0300

    A

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bla::Application.config.secret_token = 'caa138ba76c960b8623023aa4aecff5bfbabd0089fa6db79cc3b1e2478f8e565c2c2cbd11405a482e14db2c8b631b5df62aea0b6f78ad347fc9633dc80060f78'
